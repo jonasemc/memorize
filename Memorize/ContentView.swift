c3f7973a275fb8d7b63dba3378c71c522d444d6b
Jonas Miranda <jonasmirandaa21@gmail.com>
2021-09-23 20:53:18 -0300

    Criacao primeiro card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello, world")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
